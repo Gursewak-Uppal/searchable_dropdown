202ab97bd9fc8928e0cdde66ebf3f346ddcf502c
Gursewak Singh <>
2023-07-17 11:31:07 +0530

    import model

--- lib/searchable_paginated_dropdown.dart
+++ lib/searchable_paginated_dropdown.dart
@@ -4,3 +4,4 @@ library searchable_paginated_dropdown;
 export 'src/model/model.dart';
 export 'src/searchable_dropdown.dart';
 export 'src/searchable_dropdown_form_field.dart';
+export 'src/model/paginated_request_response.dart';
